200bb7cc5ce9b3108449e6e1fcbdd5bbb634ea82
Elenilson <elenilsonsantos9@gmail.com>
2022-08-23 01:26:13 -0300

    exercicios resolvidos

---
+++ script.js
@@ -0,0 +1,47 @@
+
+// let num = Number(prompt('digite um numero. para sair digite 0'))
+// let soma = 0
+
+// while(num !== 0){
+//     soma = soma + num
+// //  ou soma+= num
+//     num = Number(prompt('digite um numero. para sair digite 0'))
+// }
+// alert(`a soma dos numeros é: ${soma}`)
+// console.log(soma)
+
+// let num = Number(prompt('insira um numero'))
+
+// for(let i = 0; i <= num; i++){
+//     console.log(i)
+// }
+
+// const array = [8,7,6,5,4,3,2,1]
+
+// for(let i =0; i< array.length; i++){
+//     console.log(`o indice ${i} é: ${array[i]}`)
+// }
+
+// execicio 1
+// let nome = prompt(`digite o tipo de usuário: \n A:admin \n B:usuário comun \n C:usário assinante`).toUpperCase()
+
+// while(nome !== 'A'){
+//     alert('acesso negado')
+//     nome = prompt(`digite o tipo de usuário: \n A:admin \n B:usuário comun \n C:usário assinante`).toUpperCase()
+// }
+// alert(`boas vindas, Admin`)
+
+// exercicio 2
+
+// let num 
+
+// for(let i= 0; i <=10 ; i++){
+//     num = i*2
+//     console.log(num);
+// }
+
+// let array = ['elenilson', 'marleide', 'ana']
+
+// for(let i =0; i< array.length; i++){
+//     console.log(array[i].toUpperCase())
+// }
